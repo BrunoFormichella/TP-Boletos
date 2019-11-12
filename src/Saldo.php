0c81077f331abb360c5d0080f90008a4fcd00b5a
Dante Aguirre <dantesaguirre@gmail.com>
2019-11-11 21:43:24 -0300

    actualizados precios en Saldo, cambios en función de trasbordo

--- src/Saldo.php
+++ src/Saldo.php
@@ -17,23 +17,23 @@ class Saldo {
    *   Monto de la recarga
    */
 
-    public static function validarRecarga($monto) {
-        switch ($monto) {
-          case 10:
-          case 20:
-          case 30:
-          case 50:
-          case 100:
-            break;
-          case 510.15:
-            $monto += 81.93;
-            break;
-          case 962.59:
-            $monto += 221.58;
-            break;
-          default:
-            $monto = 0;
-        }
-        return $monto;
-      }
+  private function recargaValida($monto) {
+    switch ($monto) {
+      case 10:
+      case 20:
+      case 30:
+      case 50:
+      case 100:
+        break;
+      case 1119.90:
+        $monto += 180.10;
+        break;
+      case 2114.11:
+        $monto += 485.89;
+        break;
+      default:
+        $monto = 0;
+    }
+    return $monto;
+  }
 }
